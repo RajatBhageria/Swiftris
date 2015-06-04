ee54df7600846e3e42bf55663c7e7eeeaacc4901
Rajat Bhageria <rajat1996@gmail.com>
2015-06-04 16:16:14 -0400

    Initial Commit

---
+++ Swiftris/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  Swiftris
+//
+//  Created by Rajat Bhageria on 6/4/15.
+//  Copyright (c) 2015 Rajat Bhageria. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+    
+    required init (coder aDecoder: NSCoder){
+        fatalError("NSCoder not supported")
+    }
+    
+    override init (size: CGSize){
+        super.init(size:size)
+    
+        anchorPoint = CGPoint(x:0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x:0,y:0)
+        background.anchorPoint = CGPoint(x:0, y:1.0)
+        addChild(background)
+        
+    }
+}
